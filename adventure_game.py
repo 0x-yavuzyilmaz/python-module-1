a5fbc646113432dc152116066c390ad7a3867a24
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-28 02:13:01 +0300

    feat: Create initial structure for adventure game

---
+++ adventure_game.py
@@ -0,0 +1,7 @@
+print("Macera Oyunumuza Hoşgeldin!\n\n")
+print("""Gizemli bir odada uyanıyorsun.
+Odada eski bir ahşap kapı ve üzerinde parlayan bir sandık var.
+Kuzey duvarında ise küçük bir pencere asılı...""")
+
+eylem = input("Ne yapmak istersin? Hangi eşya ile başlayalım? Kapı, sandık, pencere?").lower()
+
