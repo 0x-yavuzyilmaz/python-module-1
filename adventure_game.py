f6913c13474a2bb4fc5135d288262df58fb241c1
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-28 18:07:09 +0300

    refactor: Finalize and clean up the game code

--- adventure_game.py
+++ adventure_game.py
@@ -1,25 +1,25 @@
-#oyunun başlangıç kısmı
-print("Macera Oyunumuza Hoşgeldin!\n\n")
+# oyunun başlangıç kısmı
+print("Macera Oyunumuza Hoşgeldin!\n")
 print("""Gizemli bir odada uyanıyorsun.
 Odada eski bir ahşap kapı ve üzerinde parlayan bir sandık var.
-Kuzey duvarında ise küçük bir pencere asılı...""")
+Kuzey duvarında ise küçük bir pencere asılı...
+""")
 
-eylem = input("Ne yapmak istersin? Hangi eşya ile başlayalım? Kapı, sandık, pencere?").lower()
+eylem = input("Ne yapmak istersin? Hangi eşya ile başlayalım? Kapı, sandık, pencere?\n").lower()
 
-
-#oyundaki if-elif-else karar yapısı
+# oyundaki if-elif-else karar yapısı
 if eylem == "kapı":
     print("Kapı kilitli görünüyor. Belki bir anahtara ihtiyacın vardır.")
 elif eylem == "sandık":
     print("Sandık da kilitli. Üzerinde küçük, paslı bir kilit var. Belki onu açacak bir şey bulabilirsin.")
 elif eylem == "pencere":
     print("Pencereden dışarı baktığında sık bir orman görüyorsun. Pencerenin pervazında ise küçük, parlak bir anahtar "
-          "duruyor!")
+          "duruyor!\n")
+    anahtar_alindi_mi = input("Evet doğru yoldasın, sandığı açman için ne gerekiyor?\n").lower()
+    if anahtar_alindi_mi == "anahtar":
+        print("Anahtarı kullanarak sandığı açtın ve içinden bir levye çıktı!")
+    else:
+        print("Hayır.Sandık hala kilitli:(")
 else:
-    print(f"{eylem} ile ne yapacağımı bilmiyorum.Lütfen 'kapı', 'sandık' veya 'pencere' gibi nesnelerle etkileşime geç. ")
-
-anahtar_alindi_mi=input("Evet doğru yoldasın, sandığı açman için gerekiyor").lower()
-if anahtar_alindi_mi == "anahtar":
-    print("Anahtarı kullanarak sandığı açtın ve içinden bir levye çıktı!")
-else:
-    print("Sandık hala kilitli:(")
+    print(
+        f"{eylem} ile ne yapacağımı bilmiyorum.Lütfen 'kapı', 'sandık' veya 'pencere' gibi nesnelerle etkileşime geç. ")
