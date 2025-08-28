6ec3b037e01fa11bb30a342f63439b049dd654d1
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-28 18:02:08 +0300

    feat: Implement decision logic for room objects

--- adventure_game.py
+++ adventure_game.py
@@ -1,3 +1,4 @@
+#oyunun başlangıç kısmı
 print("Macera Oyunumuza Hoşgeldin!\n\n")
 print("""Gizemli bir odada uyanıyorsun.
 Odada eski bir ahşap kapı ve üzerinde parlayan bir sandık var.
@@ -5,3 +6,20 @@ Kuzey duvarında ise küçük bir pencere asılı...""")
 
 eylem = input("Ne yapmak istersin? Hangi eşya ile başlayalım? Kapı, sandık, pencere?").lower()
 
+
+#oyundaki if-elif-else karar yapısı
+if eylem == "kapı":
+    print("Kapı kilitli görünüyor. Belki bir anahtara ihtiyacın vardır.")
+elif eylem == "sandık":
+    print("Sandık da kilitli. Üzerinde küçük, paslı bir kilit var. Belki onu açacak bir şey bulabilirsin.")
+elif eylem == "pencere":
+    print("Pencereden dışarı baktığında sık bir orman görüyorsun. Pencerenin pervazında ise küçük, parlak bir anahtar "
+          "duruyor!")
+else:
+    print(f"{eylem} ile ne yapacağımı bilmiyorum.Lütfen 'kapı', 'sandık' veya 'pencere' gibi nesnelerle etkileşime geç. ")
+
+anahtar_alindi_mi=input("Evet doğru yoldasın, sandığı açman için gerekiyor").lower()
+if anahtar_alindi_mi == "anahtar":
+    print("Anahtarı kullanarak sandığı açtın ve içinden bir levye çıktı!")
+else:
+    print("Sandık hala kilitli:(")
