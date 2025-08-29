fe1a8dcb568a1cf3d8b3f2458280e40b5f82d0a5
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-29 19:42:25 +0300

    feat: implement welcome screen for to-do list program

---
+++ todo_list.py
@@ -0,0 +1,13 @@
+sonlandirici = None
+yapilacaklar = []
+while not sonlandirici:
+    print(""" 
+    --- YAPILACAKLAR LİSTESİ ---
+1. Görevleri Göster
+2. Görev Ekle
+3. Görev Tamamla
+4. Görev Sil
+5. Çıkış
+---------------------------""")
+
+
