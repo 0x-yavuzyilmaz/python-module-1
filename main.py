538e233380f7d18148b94b060363f7ca58b3bd9d
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-29 17:56:51 +0300

    feat: exercise solutions initial tuple type

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
-# Alıştırma 1
+# Alıştırma 1 (Kolay): Sabit Veriler
 
-yasakli_kelimeler = ["reklam", "spam", "virüs", "bedava"]
+ayarlar = ("2.1", "Türkçe", "Koyu")
+print("Tuple'ın ikinci elemanı:", ayarlar[1])
+# Bu verilerin herhangi bir şekilde değişmesini istemediğimiz için tuple veri tipi kullanılmalıdır.
 
-cumle = "bu bir spam mail değil sadece bedava ürün tanıtımı"
-kelimeler = cumle.split()
-sayac = 0
+# Alıştırma 2 (Orta Zorluk): Veri Yapısı Dönüşümleri
 
-for kelime in kelimeler:
-    sayac += 1
-    if kelime in yasakli_kelimeler:
-        print(f"Yasaklı kelime tespit edildi: {kelime}")
-        break
-    if sayac == len(kelimeler):
-        print("Yasaklı kelime bulunamadı")
 
-# Alıştırma 2
-veri_akisi = [10, 20, -5, 30, -15, 40, 50, -2]
-pozitif_sayilar = []
-toplam = 0
-for sayi in veri_akisi:
-    if sayi < 0:
-        continue
-    pozitif_sayilar.append(sayi)
+gunler_tuple = ("Pzt", "Sal", "Çar", "Per", "Cum")
+gunler_liste = list(gunler_tuple)
+gunler_liste.extend(["Cmt", "Pzr"])
+yeni_tuple= tuple(gunler_liste)
+print("Tuple'a yeni günlerin eklenmiş hali:\n", yeni_tuple)
 
-for pozitif_sayi in pozitif_sayilar[:3]:
-    toplam += pozitif_sayi
 
-print("Pozitif Sayılar:", pozitif_sayilar)
-print("Toplam:", toplam)
+# Alıştırma 3 (İleri Seviye): Değişmezliğin Sınırları
 
-# Alıştırma 3
-yapilacaklar = []
-while True:
-    gorev = input("\n Ne yapmak istersin? (ekle / sil / göster / çıkış):").lower()
-    if gorev == "ekle":
-        yapilacaklar.append(input("Eklenecek görevi yaz: "))
-    elif gorev == "sil":
-        silinecek_gorev = input("Silinecek görevi yaz: ")
-        if silinecek_gorev in yapilacaklar:
-            yapilacaklar.remove(silinecek_gorev)
-        else:
-            print("Böyle bir görev bulunamadı.")
-    elif gorev == "göster":
-        print(f"Yapılacaklar Listesi:\n {yapilacaklar}")
-    elif gorev == "çıkış":
-        print("Çıkış yapıldı.")
-        break
-    else:
-        print("Geçersiz Seçim")
+
+ogrenci_verisi = ("Ahmet Yılmaz", 22, ["Matematik", "Fizik"])
+#ogrenci_verisi[1] = 23
+#tuple veri tipi atamaya izin vermediğinden TypeError hatası aldık.
+
+ogrenci_verisi[2].append("Kimya")
+print(ogrenci_verisi)
+
+#kod hata vermedi. Çünkü ogrenci_verisi[2].append("Kimya") dediğimizde artık tuple değil tuple içindeki liste elemanına
+#erişmiş olduğumuzdan .append metodu çalışır.
