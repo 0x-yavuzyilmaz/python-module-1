e21a4cc4038c105107e61f008a0278d3234e8082
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-30 19:50:46 +0300

    feat: implement return statement exercises

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
-# Alıştırma 1
-def menuyu_goster():
-    print("""
-        --- YAPILACAKLAR LİSTESİ ---
-    1. Görevleri Göster
-    2. Görev Ekle
-    3. Görev Tamamla
-    4. Görev Sil
-    5. Çıkış
-    ---------------------------""")
+# Alıştırma 1 (Kolay): Alan Hesaplayıcı
+
+def dikdortgen_alani_hesapla(kisa_kenar, uzun_kenar):
+    return kisa_kenar * uzun_kenar
 
 
-menuyu_goster()
-menuyu_goster()
+hesaplanan_alan = dikdortgen_alani_hesapla(5, 8)
+
+print(f"Kenarları 5 ve 8 olan dikdörtgenin alanı: {hesaplanan_alan}")
 
 
-# Alıştırma 2
-
-def ortalama_hesapla(notlar_listesi):
-    ortalama = sum(notlar_listesi) / len(notlar_listesi)
-    print(f"Notların ortalaması: {ortalama}")
-
-
-ogrenci1_notlari = [80, 90, 100]
-ogrenci2_notlari = [65, 75, 50, 85]
-
-ortalama_hesapla(ogrenci1_notlari)
-ortalama_hesapla(ogrenci2_notlari)
+# Alıştırma 2 (Orta Zorluk): Liste Analizi
+def liste_analiz_et(sayi_listesi):
+    if not sayi_listesi:
+        return 0, 0
+    else:
+        return min(sayi_listesi), max(sayi_listesi)
 
 
-# Alıştırma 3
+rakamlar = [9, 1, 5, 22, 13, 8]
 
-def profil_karti_olustur(isim: str, dogum_yili: int, meslek: str):
-    yas = 2025 - dogum_yili
+en_kucuk, en_buyuk = liste_analiz_et(rakamlar)
 
-    print(f"""
-=========================
-PROFİL KARTI
--------------------------
-İsim    : {isim.title()}
-Meslek  : {meslek.title()}
-Yaş     : {yas}
-=========================
-    """)
+print(f"Listenin en küçük elemanı {en_kucuk}, en büyük elemanı {en_buyuk}.")
 
 
-profil_karti_olustur("Yavuz", 1986, "Matematik Öğretmeni")
-profil_karti_olustur("Cemil", 1960, "emekli")
+def gecme_durumu_hesapla(vize_notu, final_notu):
+    ortalama = vize_notu * 0.4 + final_notu * 0.6
+    if ortalama >= 50:
+        return "Geçti"
+    else:
+        return "Kaldı"
+
+
+sonuc = gecme_durumu_hesapla(50, 80)
+print(f"Öğrencinin vizesi 50, finali 80 ise geçme durumu: {sonuc}")
+
+# Aşağıdaki durum başarılı olacaktır.Çünkü gecme_durumu_hesapla(30, 40) -> "Kaldı" strinigini çıktı verecektir
+# "Kaldı"=="Kaldı" True olacağından if bloku print("Test başarılı!") çalışaacaktır.
+if gecme_durumu_hesapla(30, 40) == "Kaldı": print("Test başarılı!")
