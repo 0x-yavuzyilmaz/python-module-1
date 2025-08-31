e86f5f87001f6523cc56543020bb248dfc9f50f4
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-08-31 19:25:02 +0300

    feat: implement exercises for global scope and interaction

--- main.py
+++ main.py
@@ -1,85 +1,80 @@
-from typing import List, Tuple, Union
+# Alıştırma 1 (Kolay): Kapsamları Gözlemleme
+
+kullanici_adi = "Global Admin"
 
 
-# Alıştırma 1
-def dikdortgen_alani_hesapla(kisa_kenar: float, uzun_kenar: float) -> float:
-    """Kenarları verilen bir dikdörtgenin alanını hesaplar.
-
-    Kısa ve uzun kenarları float, cinsinden verilen dikdörgenin alanını return ile döndürür.
-
-    Args:
-        kisa_kenar ( float): Dikdörtgenin kısa kenarı
-        uzun_kenar (float): Dikdörtgenin uzun kenarı
-
-    Returns:
-        float: dikdörtgenin alanını döndürür.
-
-    """
-    return kisa_kenar * uzun_kenar
+def lokal_test():
+    kullanici_adi = "Lokal Kullanıcı"
+    print(f"Fonksiyon içindeki kullanıcı: {kullanici_adi}")
 
 
-print(dikdortgen_alani_hesapla(5, 5))
-# çıktı:25
-print(dikdortgen_alani_hesapla("beş", 10))  # uyarı: Expected type 'float', got 'str' instead
+lokal_test()
+print(f"Fonksiyon dışındaki kullanıcı: {kullanici_adi}")
 
+# ÇIKTI
+# Fonksiyon içindeki kullanıcı: Lokal Kullanıcı
+# Fonksiyon dışındaki kullanıcı: Global Admin
 
-# çıktı:beşbeşbeşbeşbeşbeşbeşbeşbeşbeş
+# Fonksiyon içindeki ve dışındaki çıktıların farklı olmasının sebebi local scope içerisinde 'kullanici_adi'
+# değişkenin tanımlanmış olmasından kaynaklanır. Çünkü python önce fonksiyom içerisindeki tanımlamalara bakar
+# eğer globalde olan bir değişken lokalde tanımlanmışsa onu kullanır.
+# ama yine de kötü uygulama gölgeleme kusuru: Shadows name 'kullanici_adi' from outer scope
 
 # Alıştırma 2
 
-def liste_analiz_et(sayi_listesi: List[int]) -> Tuple[int, int]:
-    """Verilen listedeki sayıların en küçük ve en büyük olanını döndürür.
-
-    Verilen listedeki verilerin en küçük ve en büyük olanlarını sırasıyla döndürür.Eğer liste boş ise 0 ve 0
-    değerlerini döndürür.
-
-    Args:
-        sayi_listesi(List[int]): En küçük ve en büyüğün bulunacağı sayıs listesi.Veriler int olmalıdır.
-
-    Returns:
-        Tuple[int,int]: En küçük ve en büyük veriyi sırasıyla int olarak döndürüt. Liste boş ise 0 ve 0 döndürür.
-
-    """
-    if not sayi_listesi:
-        return 0, 0
-    else:
-        return min(sayi_listesi), max(sayi_listesi)
+islem_sayaci = 0
 
 
-liste: List[int] = [10, 2, 88, -5]
-
-minimum, maximum = liste_analiz_et(liste)
-print(f"Listedeki en küçük sayı: {minimum}, en büyük sayı: {maximum}")
-
-help(liste_analiz_et)
+def islem_yap_global():
+    global islem_sayaci
+    islem_sayaci += 1
+    return islem_sayaci
 
 
-# Alıştırma 3
+print(f"1. sefer: {islem_yap_global()}")
+print(f"2. sefer: {islem_yap_global()}")
+print(f"3. sefer: {islem_yap_global()}")
+
+islem_sayaci = 0
+print("-" * 20)
 
 
-# Orijinal Hali
-def gorevi_tamamla(gorevler_listesi: List[List[Union[str, bool]]], gorev_numarasi: int) -> Union[bool, None]:
-    """Liste şeklinde tamamlancak görevler ve görev numarası verildiğinde verilen görevleri tamamlar.
-
-    Liste şeklinde, bu listeninde string ve bool şeklinde bir listeden oluştuğunu göze alarak belirtilen görevi
-    tamamlar ve True olarak döndürür. Eğer görev numarası yanlışsa None değeri döndürür.
-
-    Args:
-        gorevler_listesi(List[List[Union[str, bool]]]): Görev listesi bir listeden, bu listede Görev ve Görevin
-        tamamlanıp tamamlanmadığını belirten True veya False değerlerinden oluşur.
-        gorev_numarasi: Görev numarası tamsayıdan oluşur.
-
-    Returns:
-        bool veya None: Görev tamamlandıysa True, görev numarası hatalıysa None değeri
-
-    """
-    indeks = gorev_numarasi - 1
-    if 0 <= indeks < len(gorevler_listesi):
-        gorevler_listesi[indeks][1] = True
-        return True
-    else:
-        return None  # Hata durumunda None döndürelim
+def islem_yap_return(mevcut_sayac):
+    mevcut_sayac += 1
+    return mevcut_sayac
 
 
-yapilacaklar = [["Ekmek al", False], ["Sütü ısıt", False]]
-print(gorevi_tamamla(yapilacaklar, 1))
+for i in range(3):
+    islem_sayaci = islem_yap_return(islem_sayaci)
+    print(islem_sayaci)
+
+# ÇIKTI:
+# 1. sefer: 1
+# 2. sefer: 2
+# 3. sefer: 3
+# --------------------
+# 1
+# 2
+# 3
+
+# return yöntemi kesinlikle dah öngörülebilir. Çünkü diğer türlü daha önce fonksiyonun kaç kere çalıştığını
+# bilemediğmiz durumlarda global değişkenin(islem_sayaci)' nin ne olduğu hakkında bir fikrimiz bulunmuyor.
+# hataya çok açık.
+
+CONFIG = ("production", "192.168.1.1", 8080)
+
+
+def veritabani_baglan():
+    print(f"{CONFIG[1]}:{CONFIG[2]} adresindeki veritabanına bağlanılıyor...")
+
+
+def log_mesaji_yaz(mesaj):
+    print(f"[{CONFIG[0]}] MODU: {mesaj}")
+
+
+veritabani_baglan()
+log_mesaji_yaz("Bağlantı başarılı!")
+
+# global anahtar kelimesi kullanmamıza gerek kalmamasının sebebi bu tuple'saki değişkenler lokal scope da
+# tanımlanmadığı için zaten fonksiyon içerisinden ulaşılabilir haldedir. Buradaki veriler tuple içerisinde
+# olduğundan zaten değiştirilme imkanı da bulunmamaktadır.
